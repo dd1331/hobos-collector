5c2311af01f98bafeadce91f246c033b5c999872
Chalie Choi <ddd1331@gmail.com>
2021-11-10 01:27:48 +0900

    feature: collectosService getAccessCode/getResidentInfo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CollectorsService } from './collectors/collectors.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CollectorsService],
 })
 export class AppModule {}
